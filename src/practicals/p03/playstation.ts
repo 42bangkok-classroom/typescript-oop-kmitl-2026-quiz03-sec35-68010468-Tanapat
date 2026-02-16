3e3ea226bec87befca8c3ad8968e5dac9951ba36
68010468-Tanapat <68010468@kmitl.ac.th>
2026-02-16 10:17:29 +0700

    4

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -3,20 +3,17 @@ import { Product } from './product'
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
   generation:number;
-  N:string = '';
   profile:string = '';
   discount:number = 0;
-  
 
   constructor(name:string,generation:number,price:number){
-    super()
-    this.N = name;
+    super(name)
     this.generation = generation;
     this.price = price;
   }
 
   getProfile(){
-    this.profile = `PS${this.N} (Gen ${this.N})`
+    this.profile = `PS${this.generation} (Gen ${this.generation})`
     return this.profile
   }
   getDiscountPrice(){
