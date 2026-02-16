4ff52ea5ea12c28c05aab8a450bbab92f02ca320
68010468-Tanapat <68010468@kmitl.ac.th>
2026-02-16 10:09:49 +0700

    3

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,25 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+  generation:number;
+  N:string = '';
+  profile:string = '';
+  discount:number = 0;
+  
+
+  constructor(name:string,generation:number,price:number){
+    super()
+    this.N = name;
+    this.generation = generation;
+    this.price = price;
+  }
+
+  getProfile(){
+    this.profile = `PS${this.N} (Gen ${this.N})`
+    return this.profile
+  }
+  getDiscountPrice(){
+    this.discount = this.price -((this.price * Product.DISCOUNT_PERCENT)/100);
+    return this.discount;
+  }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,18 @@
-export class Product {}
+export class Product {
+    readonly name:string ='';
+    protected price:number = 0;
+    static readonly DISCOUNT_PERCENT:number = 10;
+
+    constructor(name?:string,price?:number){
+        name ? this.name = name : name;
+        price ? this.price = price : price;
+    }
+
+   getPrice(){
+    return this.price
+   }
+    setPrice(price: number){
+        this.price = price;
+    }
+}
 
