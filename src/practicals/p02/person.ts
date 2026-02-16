d7fe411ecd16b6a830970ab511bcde1e9372bef0
68010468-Tanapat <68010468@kmitl.ac.th>
2026-02-16 09:32:35 +0700

    2

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,20 @@
-export class Person {}
+export class Person {
+    firstname:string = '';
+    lastname:string = '';
+    age:number = 0;
+    Fullname:string = '';
+    static COUNTRY:string = `Thailand`;
+
+    setAge(x:number){
+        this.age =x;
+    }
+    getFullName(){
+        this.Fullname = `${this.firstname} ${this.lastname}`
+        return this.Fullname;
+    }
+    getAge(){
+        return this.age;
+    }
+
+}
+
